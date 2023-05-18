d93f20a0bd934b499e1a43e7e1e3f790945fa147
qian2z <98630339+qian2z@users.noreply.github.com>
2023-05-18 20:55:05 +0800

    Update

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: process.env.RAWG_API_KEY,
+    key: "63b35dc062d849ee9523daae3248e780",
   },
 });
