4fc94495ac39659fbc51bb39413f39feee922709
qian2z <98630339+qian2z@users.noreply.github.com>
2023-09-05 19:29:48 +0800

    Handle Errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
 import GameDetailsPage from "./pages/GameDetailsPage";
 import Homepage from "./pages/Homepage";
 import Layout from "./pages/Layout";
@@ -7,9 +8,13 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Homepage /> },
-      { path: "games/:id", element: <GameDetailsPage /> },
+      {
+        path: "games/:id",
+        element: <GameDetailsPage />,
+      },
     ],
   },
 ]);
